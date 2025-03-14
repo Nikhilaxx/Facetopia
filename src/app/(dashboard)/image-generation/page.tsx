45f8c5e0b43619432d04ea5c6cdbc62778a2750b
Nikhilaxx <nikhish07@gmail.com>
2025-03-15 00:57:32 +0530

    Optimized image rendering and updated layout responsiveness

--- src/app/(dashboard)/image-generation/page.tsx
+++ src/app/(dashboard)/image-generation/page.tsx
@@ -1,15 +1,16 @@
 import Configurations from '@/components/image-generation/Configurations'
+import GeneratedImages from '@/components/image-generation/GeneratedImages'
 import React from 'react'
 
 
 const ImageGeneration = () => {
   return (
-    <section className='container mx-auto grid gap-4 grid-cols-3 overflow-hidden'>
-      <div  className='ml-10 -mt-0.8' >
+    <section className='container mx-auto grid gap-5 grid-cols-3 overflow-hidden'>
+      <div  className='ml-10 -mt-0.8 col-span-1 w-[500px]' >
      <Configurations  />
       </div>
-      <div className='col-span-2 p-4 rounded-xl flex items-center justify-center'>
-        Output Images
+      <div className='col-span-2 p-4 rounded-xl flex items-center justify-center h-fit'>
+      <GeneratedImages />
       </div>
     </section>
   )
